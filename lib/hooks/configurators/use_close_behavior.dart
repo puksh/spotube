31a7f634c01a7ad02dd617264120b300af356690
Lucas <85502182+puksh@users.noreply.github.com>
2026-03-07 18:32:24 +0100

    chore(use_close_behavior and navigation_bar): remove unused imports

--- lib/hooks/configurators/use_close_behavior.dart
+++ lib/hooks/configurators/use_close_behavior.dart
@@ -1,12 +1,9 @@
-import 'dart:async';
-
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:spotube/hooks/configurators/use_window_listener.dart';
 import 'package:spotube/models/database/database.dart';
 import 'package:spotube/provider/user_preferences/user_preferences_provider.dart';
 import 'package:spotube/services/app_exit/app_exit.dart';
 import 'package:spotube/services/notifications/background_close_notification.dart';
-import 'package:spotube/utils/platform.dart';
 import 'package:window_manager/window_manager.dart';
 
 void useCloseBehavior(WidgetRef ref) {
--- lib/modules/root/spotube_navigation_bar.dart
+++ lib/modules/root/spotube_navigation_bar.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart' show Badge;
 import 'package:flutter_hooks/flutter_hooks.dart';
